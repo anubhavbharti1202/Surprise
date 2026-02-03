4f5860566f5e0a953bb1f54715f140ffc40edf72
Anubhav Bharti <anubhavbharti266@gmail.com>
2026-02-03 20:05:28 +0530

    deleted

--- types.ts
+++
@@ -1,9 +0,0 @@
-
-export enum AppState {
-  INTRO = 'INTRO',           // GF sees initial message
-  QUESTION = 'QUESTION',     // Asking the big question
-  ACCEPTED = 'ACCEPTED',     // She clicked Yes
-  REJECTED = 'REJECTED'      // She clicked No
-}
-
-export type KiwiMood = 'curious' | 'happy' | 'sad' | 'dancing' | 'hiding';
